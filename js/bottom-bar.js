d52813aacc2742309761c3ea4c712729e870cb6c
cylorun <109686521+cylorun@users.noreply.github.com>
2023-09-15 12:50:41 +0100

    languste

--- js/bottom-bar.js
+++ js/bottom-bar.js
@@ -1,4 +1,4 @@
-fetch('bottom-bar.html')
+fetch('./presets/bottom-bar.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('bottom-bar').innerHTML = data;
--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,4 @@
-fetch('navbar.html')
+fetch('./presets/navbar.html')
 .then(response => response.text())
 .then(data => {
     document.getElementById('navbar').innerHTML = data;
